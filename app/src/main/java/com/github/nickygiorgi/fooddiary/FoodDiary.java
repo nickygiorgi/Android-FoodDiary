b01567a8d6425b23793a67c60ab8d02b6ef3dbf7
Nicole Giorgi <nicolegiorgi@gmail.com>
2016-03-01 21:33:14 +0000

    changed main activity to have toolbar
    toolbar added showing app name + separated content layout xml

--- app/src/main/java/com/github/nickygiorgi/fooddiary/FoodDiary.java
+++ app/src/main/java/com/github/nickygiorgi/fooddiary/FoodDiary.java
@@ -2,6 +2,7 @@ package com.github.nickygiorgi.fooddiary;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -14,6 +15,8 @@ public class FoodDiary extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food_diary);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 
     @Override
