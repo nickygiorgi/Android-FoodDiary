6076450e0e35fd0a4d20c4d3e6ca863e8a1cf273
Nicole Giorgi <nicolegiorgi@gmail.com>
2016-05-03 21:13:15 +0100

    draft of listview for list history activity

--- app/src/main/java/com/github/nickygiorgi/fooddiary/ListHistoryActivity.java
+++ app/src/main/java/com/github/nickygiorgi/fooddiary/ListHistoryActivity.java
@@ -1,11 +1,19 @@
 package com.github.nickygiorgi.fooddiary;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class ListHistoryActivity extends AppCompatActivity {
 
@@ -13,8 +21,45 @@ public class ListHistoryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_history);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
+        final ListView listview = (ListView) findViewById(R.id.listview);
+        String[] values = new String[] { "yesterday - bad", "today - ok" };
+
+        final ArrayList<String> list = new ArrayList<String>();
+        for (int i = 0; i < values.length; ++i) {
+            list.add(values[i]);
+        }
+        final StableArrayAdapter adapter = new StableArrayAdapter(
+                this,
+                android.R.layout.simple_list_item_1,
+                list);
+        listview.setAdapter(adapter);
     }
+
+    private class StableArrayAdapter extends ArrayAdapter<String> {
+
+        HashMap<String, Integer> mIdMap = new HashMap<String, Integer>();
+
+        public StableArrayAdapter(Context context, int textViewResourceId,
+                                  List<String> objects) {
+            super(context, textViewResourceId, objects);
+            for (int i = 0; i < objects.size(); ++i) {
+                mIdMap.put(objects.get(i), i);
+            }
+        }
+
+        @Override
+        public long getItemId(int position) {
+            String item = getItem(position);
+            return mIdMap.get(item);
+        }
+
+        @Override
+        public boolean hasStableIds() {
+            return true;
+        }
+
+    }
+
 
 }
